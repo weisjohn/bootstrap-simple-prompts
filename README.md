cf9c35cc2d8886078f893e7b8f47671f2e5c3d73
John Weis <weis.john@gmail.com>
2014-01-15 13:16:15 -0500

    initial checkin, lots more todo

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ bootstrap-simple-prompts
 ========================
 
 Bootstrap Modals for `alert()` and `confirm()`
+
+Requires Bootstrap 3.0+ and jQuery.
+
+### usage
+
+`alert(message, [title])`
+
+// TODO: image here...
+
+`confirm(message, [title], callback)`
+
+// TODO: image here as well
+
+
+
+
+#### credits
+
+Originally, this was a fork of sairam's [bootstrap-prompts](https://github.com/sairam/bootstrap-prompts), but after significant departure, I rewrote it.
---
+++ boostrap-simple-prompts.js
@@ -0,0 +1,68 @@
+(function() {
+
+    // ensure depenencies
+    if (!$.fn.modal.Constructor) return;
+
+    // gross helper function to generate markup in a quick way
+    function generate(text, title, confirm) {
+        var markup = '\
+<div class="modal fade" id="bootstrap-prompts-modal"> \
+    <div class="modal-dialog"> \
+        <div class="modal-content"> \
+'
+        if (title) {
+            markup += '\
+            <div class="modal-header"> \
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button> \
+                <h4 class="modal-title">' + (title) + '</h4> \
+            </div> \
+            '
+        }
+
+        markup += '\
+            <div class="modal-body"> \
+                <p>' + (text || "") + '</p> \
+            </div> \
+            <div class="modal-footer"> \
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>';
+        if (confirm) markup += '<button type="button" class="btn btn-primary">Ok</button>';
+        markup += '\
+            </div> \
+        </div> \
+    </div> \
+</div> \
+';
+        return markup;
+    }
+
+
+    function override(method, fn) {
+        if (!window[method]) {
+            console.error("window." + method + " is undefined");
+            return;
+        } 
+        if (typeof fn !== "function") {
+            console.error("fn must be a function");
+            return;
+        }
+        fn.original = window[method];
+        window[method] = fn;
+    }
+
+    
+    function spawn(msg, title, confirm) {
+        console.log(msg)
+        $("#bootstrap-prompts-modal").remove();
+        var $modal = $(generate(msg, title, confirm));
+        $('body').append($modal);
+        $modal.modal({ show : true });
+        // TODO: deal with confirm
+    }
+
+    override('alert', function(msg, title) { spawn(msg, title); });
+    override('confirm', function(msg, title, cb) { 
+        if (typeof title === "function") cb = title, title = null;
+        spawn(msg, title, true, cb);
+    });
+
+})();
